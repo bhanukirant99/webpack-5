02a6ad222844dfb3c6554126d7be55db0acb4525
Kushank Sriraj <kushanksriraj@gmail.com>
2021-05-15 18:12:10 +0530

    setup webpack

---
+++ src/App.js
@@ -0,0 +1,5 @@
+const App = ({ name }) => {
+  return `<h1> Hello ${name}!</h1>`;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from "./App";
+
+const root = document.getElementById("root");
+
+root.innerHTML = `Learning tooling! ${App({ name: "Kushank" })}`;
